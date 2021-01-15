1d5f302b5e9a91644ecf20ffa1afd9f739bcc002
Šarūnas <sarunas01@gmail.com>
2021-01-15 14:55:57 +0200

    init

---
+++ src/main/java/com/JobApplication/SingleIBANVerificationService/SingleIbanVerificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.JobApplication.SingleIBANVerificationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleIbanVerificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleIbanVerificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JobApplication/SingleIBANVerificationService/SingleIbanVerificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JobApplication.SingleIBANVerificationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleIbanVerificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
